eec756b909e832615c8ac3e0d8cf30aba98cd2fc
Gai <gaish@ac.sce.ac.il>
2025-01-02 10:54:26 +0200

    General Update

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+// import { StatusBar  } from "expo-status-bar";
+// import { Text, View  } from "react-native";
+// import { Link } from "expo-router";
+
+
+// export default function App() {
+
+//     return(
+//  <Link href="/"
+
+
+//     );
+// }
